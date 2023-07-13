bfc89e02d5cf9ac5de0972538c6919a67d0a5ba2
Mauricio Huayta <huaytamauricio1@gmail.com>
2023-07-13 16:34:23 -0400

    Creacion de tests para funciones de TasksContract

--- contracts/TasksContract.sol
+++ contracts/TasksContract.sol
@@ -5,6 +5,20 @@ contract TasksContract {
 
     uint public taskCounter = 0;
 
+    constructor () {
+        createTask("mi primer tarea de ejemplo","tengo que hacer algo");
+    }
+
+    event TaskCreated(
+        uint id,
+        string title,
+        string description,
+        bool done,
+        uint createdAt
+    );
+
+    event TaskToggleDone(uint id, bool done);
+
     struct Task {
         uint256 id;
         string title;
@@ -16,9 +30,15 @@ contract TasksContract {
     mapping (uint256 => Task) public tasks; // Estructura de datos tipo llave-valor
 
     function createTask(string memory _title, string memory _description) public { // 'memory' indica que no se guarde en blockchain sino se use solo memoria temporal
-        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp); // 'block' hace referencia al bloque donde se ejecuto el contrato
         taskCounter++;
+        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp); // 'block' hace referencia al bloque donde se ejecuto el contrato
+        emit TaskCreated(taskCounter, _title, _description, false, block.timestamp); // Devuelve informacion sobre la tarea creada, en los datos de transaccion
     }
 
-    // function toggleDone() {}
+    function toggleDone(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.done = !_task.done; // Cambiar el estado de tarea al contrario
+        tasks[_id] = _task;
+        emit TaskToggleDone(_id, _task.done);
+    }
 }
