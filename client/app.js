ed8a263813aba29f058b16eeac16f8c8fa2ec34b
Mauricio Huayta <huaytamauricio1@gmail.com>
2023-07-19 19:17:56 -0400

    Agrega de funciones en app.js para cargado de Ethereum en front

--- client/app.js
+++ client/app.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+App = {
+    init: () => {
+        console.log("Loaded")
+        App.loadEthereum()
+    },
+
+    loadEthereum: async () => {
+        if (window.ethereum) {
+            // console.log("Ethereum existe")
+            App.web3Provider = window.ethereum
+            await window.ethereum.request({method: 'eth_requestAccounts'})
+        } else if(window.web3) {
+            web3 = new Web3(window.web3.currentProvider)
+        } else {
+            console.log("No ethereum browser is installed. Try installing Metamask")
+        }
+    },
+
+    loadContracts: async () => {
+        
+    }
+
+
+}
+
+App.init()
